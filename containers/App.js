884c2887f7cc5edfd50e2a7133dab71a57e3bcaa
Azief <12074451+aiqbals@users.noreply.github.com>
2021-01-18 16:15:16 +0100

    Add files via upload

---
+++ containers/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+import Clock from '../components/clock/Clock';
+import CardList from '../components/cardlist/CardList';
+import Scroll from '../components/scroll/Scroll';
+import Searchbox from '../components/searchbox/Searchbox';
+//import {robots} from './robots';
+import './App.css';
+
+class App extends Component {
+    /* constructor(){
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    } */
+    state = {
+        robots: [],
+        searchfield: ''
+    }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then( res => res.json())
+        .then( users => this.setState({ robots: users }));
+    }
+
+    onSearchChange = (event) => {  
+        //console.log(event.target.value);
+        this.setState({searchfield: event.target.value});
+    }
+
+    render() {
+        const filteredRobot = this.state.robots.filter( robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+
+        return !this.state.robots.length ? <h1> Loading...</h1> :
+            (
+                <div className='tc'>
+                    <h1 className='f2'> RoboBuild Search! </h1>
+                    <Searchbox searchChange={this.onSearchChange} />
+                    <Clock />
+                    <Scroll>
+                        <CardList robots={filteredRobot} />
+                    </Scroll>
+                </div>
+            );    
+    }
+}
+
+export default App;
