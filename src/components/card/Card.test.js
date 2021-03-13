8a2885a6a37ddea6dcbbfac4a7cfa37575d89036
Asif Hoque <asif.engr13@gmail.com>
2021-03-13 20:01:31 +0100

    test cardList

--- src/components/card/Card.test.js
+++ src/components/card/Card.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
-import Card from './card';
+import Card from './Card';
 
 it('expect to render Card component alone', () => {
     //expect(shallow(<Card />).length).toEqual(1) // enzyme test
